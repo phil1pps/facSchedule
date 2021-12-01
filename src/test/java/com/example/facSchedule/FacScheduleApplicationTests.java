9aee0acc7fce60e255662b512c140ad560185c1f
PHILIPPS <sniper911185000@gmail.com>
2021-12-01 20:34:59 +0200

    smth

---
+++ src/test/java/com/example/facSchedule/FacScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

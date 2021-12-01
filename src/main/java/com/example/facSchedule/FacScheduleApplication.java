b76758eb0d418506ed4a21989bebbb0233e258d5
PHILIPPS <sniper911185000@gmail.com>
2021-12-01 20:34:59 +0200

    smth

---
+++ src/main/java/com/example/facSchedule/FacScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.facSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacScheduleApplication.class, args);
+	}
+
+}

25c5a8197b27da9a7fa6fe750c6cbf224830fbb5
Vlad Kovryzhenko <kovrud21@gmail.com>
2021-12-03 08:57:51 +0200

    zabuly controllers

---
+++ src/main/java/com/example/facSchedule/controller/ProfessorController.java
@@ -0,0 +1,9 @@
+package com.example.facSchedule.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/professor")
+public class ProfessorController {
+}

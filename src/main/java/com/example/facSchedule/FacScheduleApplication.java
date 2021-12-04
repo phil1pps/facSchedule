16298302f4fe3458a63f564390e9f3cacb536183
IvanGerasymenko <gerasymenko15@gmail.com>
2021-12-04 13:07:25 +0200

    ...

--- src/main/java/com/example/facSchedule/FacScheduleApplication.java
+++ src/main/java/com/example/facSchedule/FacScheduleApplication.java
@@ -1,11 +1,9 @@
 package com.example.facSchedule;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition
 public class FacScheduleApplication {
 
 	public static void main(String[] args) {
